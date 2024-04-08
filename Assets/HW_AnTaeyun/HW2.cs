adf310058eddd804c61d4d041fa71fc73792535a
WindowsTiger <0xodbs02@naver.com>
2024-04-08 20:22:58 +0900

    style: Change System.Math to Math

--- Assets/HW_AnTaeyun/HW2.cs
+++ Assets/HW_AnTaeyun/HW2.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System;
 using UnityEngine;
 using TaeYun;
 
@@ -10,13 +9,13 @@ namespace TaeYun
     class Entity {
         private Rigidbody2D _rigid;
         public double VelocityMS{
-            get{return System.Math.Sqrt(_rigid.velocity[0]*_rigid.velocity[0]+_rigid.velocity[1]*_rigid.velocity[1]);}
+            get{return Mathf.Sqrt(_rigid.velocity[0]*_rigid.velocity[0]+_rigid.velocity[1]*_rigid.velocity[1]);}
         }
         public double VelocityKmS{
-            get{return System.Math.Sqrt(_rigid.velocity[0]*_rigid.velocity[0]+_rigid.velocity[1]*_rigid.velocity[1])/1000f;}
+            get{return Mathf.Sqrt(_rigid.velocity[0]*_rigid.velocity[0]+_rigid.velocity[1]*_rigid.velocity[1])/1000f;}
         }
         public double VelocityKmh{
-            get{return System.Math.Sqrt(_rigid.velocity[0]*_rigid.velocity[0]+_rigid.velocity[1]*_rigid.velocity[1])*3.6f;}
+            get{return Mathf.Sqrt(_rigid.velocity[0]*_rigid.velocity[0]+_rigid.velocity[1]*_rigid.velocity[1])*3.6f;}
         }
 
 
