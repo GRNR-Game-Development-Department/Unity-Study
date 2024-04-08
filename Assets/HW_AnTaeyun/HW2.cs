6fcca436f0d737c8d423d684ef17cebef006bf18
WindowsTiger <0xodbs02@naver.com>
2024-04-08 20:19:12 +0900

    feat: 2주차 과제

---
+++ Assets/HW_AnTaeyun/HW2.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using TaeYun;
+
+namespace TaeYun
+{
+
+    class Entity {
+        private Rigidbody2D _rigid;
+        public double VelocityMS{
+            get{return System.Math.Sqrt(_rigid.velocity[0]*_rigid.velocity[0]+_rigid.velocity[1]*_rigid.velocity[1]);}
+        }
+        public double VelocityKmS{
+            get{return System.Math.Sqrt(_rigid.velocity[0]*_rigid.velocity[0]+_rigid.velocity[1]*_rigid.velocity[1])/1000f;}
+        }
+        public double VelocityKmh{
+            get{return System.Math.Sqrt(_rigid.velocity[0]*_rigid.velocity[0]+_rigid.velocity[1]*_rigid.velocity[1])*3.6f;}
+        }
+
+
+        public Entity(Rigidbody2D objectRb){
+            _rigid = objectRb;
+        }
+    }
+    public class HW2 : MonoBehaviour
+    {
+        private Entity _temp;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _temp = new Entity(GetComponent<Rigidbody2D>());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log(_temp.VelocityMS + "m/s");
+            Debug.Log(_temp.VelocityKmS + "km/s");
+            Debug.Log(_temp.VelocityKmh + "km/h");
+        }
+    }
+}
