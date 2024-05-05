58f894ccbc6639cb4801f215711f11109e37d88b
Chienu0525 <165155535+Chienu0525@users.noreply.github.com>
2024-05-05 22:06:47 +0900

    서형근 2주차 과제

---
+++ Assets/Scripts/SeoHyeongGeun.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HyeongGeunHW {
+    public class SeoHyeongGeun : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            print("Seo Hyeong Geun");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
