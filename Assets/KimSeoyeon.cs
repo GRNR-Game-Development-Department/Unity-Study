4c3437629dd6b7a3f3a0323e5378854aaed528eb
pyuhyu <155874194+pyuhyu@users.noreply.github.com>
2024-05-05 16:01:17 +0900

    idk_seoyeon
    머지

--- Assets/KimSeoyeon.cs
+++ Assets/KimSeoyeon.cs
@@ -7,7 +7,7 @@ public class KimSeoyeon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("보쿠노 나마에와 Kim Seo yeon 데스")
+        print("보쿠노 나마에와 Kim Seo yeon 데스");
     }
 
     // Update is called once per frame
